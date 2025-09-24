43ab4bd70807db356917f06359692d711186d4ee
Leonardo Sanchez <leonardosanchez119@gmail.com>
2025-09-24 09:59:22 -0300

    servicio de boton contactanos para mail realizado

--- src/app/booton-contactanos/booton-contactanos.component.ts
+++ src/app/booton-contactanos/booton-contactanos.component.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './booton-contactanos.component.css'
 })
 export class BootonContactanosComponent {
+  contactar() {
+  const destinatario = "leopedrowebsolutions@gmail.com";
+  const subject = encodeURIComponent("Solicitud de presupuesto");
+  const body = encodeURIComponent("Hola, quiero más información sobre un proyecto de software. ¿Podrían enviarme un presupuesto?");
+  
+  window.location.href = `mailto:${destinatario}?subject=${subject}&body=${body}`;
+}
 
 }
