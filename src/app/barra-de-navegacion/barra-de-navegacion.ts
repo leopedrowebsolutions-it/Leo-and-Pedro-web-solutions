99016e9fe08644a0b89588b7c676c10b9b55aa02
Leonardo Sanchez <leonardosanchez119@gmail.com>
2025-09-24 09:20:17 -0300

    agrego boton de contacto en barra de navegacion

--- src/app/barra-de-navegacion/barra-de-navegacion.ts
+++ src/app/barra-de-navegacion/barra-de-navegacion.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { BootonContactanosComponent } from "../booton-contactanos/booton-contactanos.component";
 
 @Component({
   selector: 'app-barra-de-navegacion',
-  imports: [],
+  imports: [BootonContactanosComponent],
   templateUrl: './barra-de-navegacion.html',
   styleUrl: './barra-de-navegacion.css'
 })
