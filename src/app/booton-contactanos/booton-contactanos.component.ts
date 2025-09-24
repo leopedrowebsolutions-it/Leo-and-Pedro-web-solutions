dd9c6659fffe1c9a8d5533439245f004e5deac93
Pedro Fabian Teran <teranpedrofabian@gmail.com>
2025-09-24 11:07:53 -0300

    solucion de bug en boton contactanos

--- src/app/booton-contactanos/booton-contactanos.component.ts
+++ src/app/booton-contactanos/booton-contactanos.component.ts
@@ -2,17 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-booton-contactanos',
-  imports: [],
   templateUrl: './booton-contactanos.component.html',
-  styleUrl: './booton-contactanos.component.css'
+  styleUrls: ['./booton-contactanos.component.css']
 })
 export class BootonContactanosComponent {
-  contactar() {
+contactar() {
   const destinatario = "leopedrowebsolutions@gmail.com";
   const subject = encodeURIComponent("Solicitud de presupuesto");
   const body = encodeURIComponent("Hola, quiero más información sobre un proyecto de software. ¿Podrían enviarme un presupuesto?");
-  
-  window.location.href = `mailto:${destinatario}?subject=${subject}&body=${body}`;
-}
 
+  const gmailUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=${destinatario}&su=${subject}&body=${body}` ;
+  window.open(gmailUrl, "_blank");
+}
 }
