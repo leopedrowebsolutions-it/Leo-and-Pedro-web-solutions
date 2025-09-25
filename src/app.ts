f7d8864e6e8c12ec603102828177ff67028d86ab
Pedro Fabian Teran <teranpedrofabian@gmail.com>
2025-09-25 14:53:27 -0300

    finalizacion de secion sobre nopsotros, inicio de seccion servicios

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { BarraDeNavegacion } from "./barra-de-navegacion/barra-de-navegacion";
 
 import { FooterPageComponent } from './footer-page/footer-page.component';
+import { SobreNosotros } from "./sobre-nosotros/sobre-nosotros";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BarraDeNavegacion, FooterPageComponent],
+  imports: [RouterOutlet, BarraDeNavegacion, FooterPageComponent, SobreNosotros],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
