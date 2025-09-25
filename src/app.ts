a440a2240f6a37aec05b2eeaa1af29db4a4fd848
Leonardo Sanchez <leonardosanchez119@gmail.com>
2025-09-25 15:16:44 -0300

    agrego seccion de equipo

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { BarraDeNavegacion } from "./barra-de-navegacion/barra-de-navegacion";
 
 import { FooterPageComponent } from './footer-page/footer-page.component';
+import { Equipo } from "./equipo/equipo";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BarraDeNavegacion, FooterPageComponent],
+  imports: [RouterOutlet, BarraDeNavegacion, FooterPageComponent, Equipo],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
